b439b77154db336c77e2da13478374c0579d98c4
Mayank Tripathi <tripathimayank321@gmail.com>
2023-09-09 20:56:25 +0530

    Project open in new tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,6 +22,7 @@ export default function Projects() {
             <a
               href={project.link}
               key={project.image}
+              target="_blank"
               className="sm:w-1/2 w-100 p-4"
             >
               <div className="flex relative">
