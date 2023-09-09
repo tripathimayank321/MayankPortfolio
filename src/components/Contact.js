543e6c01c3d7cd9d97b12128b0b0c1a96e49cad7
Mayank Tripathi <tripathimayank321@gmail.com>
2023-09-09 09:50:54 +0530

    Changed Map Location

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=Caledonia+Street,+Liverpool+L7+7DX,+UK&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=Ropemaker+Place,+Renshaw+Street,+Liverpool,+UK&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
