7a23dde520991e65bfec6d03daf2006f1df88800
Mayank Tripathi <tripathimayank321@gmail.com>
2023-09-09 09:26:51 +0530

    Portfolio Files added

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Mayank.
+            <br className="hidden lg:inline-block" />I like to code things from
+            scratch, and enjoy bringing ideas to life in the browser.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Currently working as IAM DevOps specialist at Ericsson. I'm skilled
+            Front End Developer learn to build awesome apps from scratch with
+            good looking UI. I've worked in Front End Tech stack primarily
+            React, Typescript, JavaScript, GIT, TDD using Jest, Node JS ,
+            Express and much more.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
